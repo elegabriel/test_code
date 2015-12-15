4b6aece5c7dcb28dd62016230fb83ab19698e894
ele <elegabriel@163.com>
2015-12-15 21:52:33 +0800

    3D_watermark modified

---
+++ C++/ichar_traits.h
@@ -0,0 +1,46 @@
+#ifndef ICHAR_TRAITS_H
+#define ICHAR_TRAITS_H
+#include <string>
+#include <cctype>
+
+struct ichar_traits : std::char_traits<char>{
+  static bool eq(char c1st, char c2nd){
+    return std::toupper(c1st) == std::toupper(c2nd);
+  }
+  static bool ne(char c1st, char c2nd){
+    return std::toupper(c1st) != std::toupper(c2nd);
+  }
+  static bool lt(char c1st, char c2nd){
+    return std::toupper(c1st) < std::toupper(c2nd);
+  }
+  static int compare(const char* str1, const char* str2, size_t n){
+    for(int i = 0; i < n; i++){
+      if (std::tolower(*str1)>std::tolower(*str2)) {
+        return 1;
+      }
+      if (std::tolower(*str1)<std::tolower(*str2)) {
+        return -1;
+      }
+      if(*str1 ==0 || *str2 == 0){
+        return 0;
+      }
+      str1++; str2++;
+    }
+    return 0;
+  }
+  static const char* find(const char* s1, int n, char c){
+    while(n-- > 0 && std::toupper(*s1) != std::toupper(c))
+      s1++;
+    return s1;
+  }
+};
+#endif
+
+typedef basic_string<char, ichar_traits, allocator<char>> istring;
+
+int main(){
+    istring first = "tHis";
+    istring second = "ThIs";
+    cout<< first.compare(second)<<endl;
+    return 0;
+}
